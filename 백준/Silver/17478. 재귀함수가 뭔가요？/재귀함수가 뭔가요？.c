76b01acb84cf5e7c859cc2758ca12ce46aa41ab9
gorodog <143953062+gorodog@users.noreply.github.com>
2023-11-13 04:32:41 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.c
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 
-void printIndent(int b) {
+void K(int b) {
     for (int i = 0; i < b; ++i) {
         printf("____");
     }
 }
 
-void recursivePrint(int n, int b) {
-    printIndent(b);
+void Y(int n, int b) {
+    K(b);
     printf("\"재귀함수가 뭔가요?\"\n");
 
     if (n == 0) {
-        printIndent(b);
+        K(b);
         printf("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
-        printIndent(b);
+        K(b);
         printf("라고 답변하였지.\n");
     }
     else {
-        printIndent(b);
+        K(b);
         printf("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
-        printIndent(b);
+        K(b);
         printf("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
-        printIndent(b);
+        K(b);
         printf("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
 
-        recursivePrint(n - 1, b + 1); // 두 문장 사이에서 재귀호출 하면 사이에 문장이 계속 추가되어 출력
+        Y(n - 1, b + 1);
 
-        printIndent(b);
+        K(b);
         printf("라고 답변하였지.\n");
     }
 }
@@ -37,7 +37,7 @@ int main() {
 
     printf("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
 
-    recursivePrint(a, 0);
+    Y(a, 0);
 
     return 0;
 }
