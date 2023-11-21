f7cd3fcc961395abe299eb0ed25c55a68b76c34b
gorodog <143953062+gorodog@users.noreply.github.com>
2023-11-21 19:40:50 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) 
+{ 
+    int n = 0; 
+    scanf("%d", &n);
+    
+    if (n == 0)
+        printf("YONSEI");
+    if (n == 1)
+        printf("Leading the Way to the Future");
+
+    return 0; 
+} 
