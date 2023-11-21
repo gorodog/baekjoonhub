5c3540f6fdabcf0c1bbf8c654500dc5d706565f8
gorodog <143953062+gorodog@users.noreply.github.com>
2023-11-21 19:19:09 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 456 ms, Memory: 8808 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a , const void *b); 
+
+int main(void) 
+{ 
+    int n = 0; 
+    int arr[1000000] = {0}; 
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    qsort(arr, n, sizeof(arr[0]), compare); 
+
+    for(int i = 0 ; i < n ; i++) 
+        printf("%d\n", arr[i]);
+    return 0; 
+} 
+
+int compare(const void *a , const void *b) 
+{ 
+     if( *(int*)a > *(int*)b )
+        return -1;
+    else if( *(int*)a < *(int*)b )
+        return 1;
+    else
+        return 0;
+}
